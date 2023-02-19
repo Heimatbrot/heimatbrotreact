8c5631a3a192183e549f94041aa2c85fe59b8e01
Heimatbrot <114741802+Heimatbrot@users.noreply.github.com>
2023-02-19 17:32:39 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
 
+function changetest(){
+  document.getElementById("changetest_paragraph").innerText = "it workz";
+  return
+}
+
 function App() {
   return (
-    <h1 class="center">React</h1>
+    <div class = "center">
+          <h1>React</h1>
+          <p id="changetest_paragraph">Versuch Nr. 1</p>
+          <button onClick={changetest}>Klick mich</button>
+    </div>
   )
 }
+
 
 export default App;
