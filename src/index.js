952024c601ad956c575ace006474be973c989a4b
Heimatbrot <114741802+Heimatbrot@users.noreply.github.com>
2023-02-19 16:28:32 +0000

    test

--- src/index.js
+++ src/index.js
@@ -4,16 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-function HelloWorld() {
-  return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
-  );
-}
-
-export default HelloWorld;
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
