dd1591610d5ca2c05e5f73ef9b26e13dc2f23f9b
Heimatbrot <114741802+Heimatbrot@users.noreply.github.com>
2023-02-20 19:59:18 +0000

    deploy 20.02.23

--- src/App.js
+++ src/App.js
@@ -2,14 +2,36 @@ import React from 'react';
 import './App.css';
 
 var turn = "X"
+const ways_win = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+
+function checkwinner(){
+  const buttons = document.querySelectorAll('.gamegrid');
+  for (let i = 0; i < ways_win.length; i++) {
+    const [a, b, c] = ways_win[i];
+    if (buttons[a].innerText === buttons[b].innerText &&
+        buttons[b].innerText === buttons[c].innerText &&
+        buttons[a].innerText !== '') {
+      document.getElementById("winner").style.display = "block"
+      document.getElementById("winner").innerText = "'" + buttons[a].innerText + "' has won!"
+      const gameGridElements = document.querySelectorAll(".gamegrid");
+        for (let i = 0; i < gameGridElements.length; i++) {
+        gameGridElements[i].disabled = true;
+        }
+      return true;
+    }
+  }
+  return false;
+}
 
 function play(field) {
   if (field.innerText!=="X" && field.innerText!=="O"){
   if (turn === "X" ) {
     field.innerText = "X"
+    checkwinner()
     turn = "O"
   } else {
     field.innerText = "O"
+    checkwinner()
     turn = "X"
   }}
   else {
@@ -21,6 +43,7 @@ function App() {
   return (
     <div className="center">
       <h1>HB React</h1><h2>Tic Tac Toe</h2>
+      <h2 id="winner">winner is</h2>
       <button className="gamegrid" id='but0' onClick={(event) => play(event.target)}> </button>
       <button className="gamegrid" id='but1' onClick={(event) => play(event.target)}> </button>
       <button className="gamegrid" id='but2' onClick={(event) => play(event.target)}> </button><br></br>
