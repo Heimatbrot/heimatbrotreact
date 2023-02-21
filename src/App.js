6a04015a12a0f8c2d84ade50326b37c27314e80b
Heimatbrot <114741802+Heimatbrot@users.noreply.github.com>
2023-02-21 09:24:04 +0000

    save

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import './App.css';
 var turn = "X"
 const ways_win = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
+//function reset() {
+//  window.location.reload()
+//}
+
 function checkwinner(){
   const buttons = document.querySelectorAll('.gamegrid');
   for (let i = 0; i < ways_win.length; i++) {
