85bd3e5f076f49b5bacf304e0ef5a1617ec25aee
Heimatbrot <114741802+Heimatbrot@users.noreply.github.com>
2023-02-22 09:44:50 +0000

    commit tie

--- src/App.js
+++ src/App.js
@@ -10,6 +10,24 @@ const ways_win = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6
 
 function checkwinner(){
   const buttons = document.querySelectorAll('.gamegrid');
+  if (buttons[0].innerText !== "" &&
+      buttons[1].innerText !== "" &&
+      buttons[2].innerText !== "" &&
+      buttons[3].innerText !== "" &&
+      buttons[4].innerText !== "" &&
+      buttons[5].innerText !== "" &&
+      buttons[6].innerText !== "" &&
+      buttons[7].innerText !== "" &&
+      buttons[8].innerText !== ""
+    ) { 
+      document.getElementById("winner").style.display = "block"
+      document.getElementById("winner").innerText = "it's a tie!"
+      const gameGridElements = document.querySelectorAll(".gamegrid");
+        for (let i = 0; i < gameGridElements.length; i++) {
+        gameGridElements[i].disabled = true;
+        document.getElementById("reset").style.display = "block"
+        }
+      }
   for (let i = 0; i < ways_win.length; i++) {
     const [a, b, c] = ways_win[i];
     if (buttons[a].innerText === buttons[b].innerText &&
@@ -53,15 +71,15 @@ function App() {
       <div>
       <h1>HB React</h1><h2>Tic Tac Toe</h2>
       <h2 id="winner">winner is</h2>
-      <button className="gamegrid" id='but0' onClick={(event) => play(event.target)}> </button>
-      <button className="gamegrid" id='but1' onClick={(event) => play(event.target)}> </button>
-      <button className="gamegrid" id='but2' onClick={(event) => play(event.target)}> </button><br></br>
-      <button className="gamegrid" id='but3' onClick={(event) => play(event.target)}> </button>
-      <button className="gamegrid" id='but4' onClick={(event) => play(event.target)}> </button>
-      <button className="gamegrid" id='but5' onClick={(event) => play(event.target)}> </button><br></br>
-      <button className="gamegrid" id='but6' onClick={(event) => play(event.target)}> </button>
-      <button className="gamegrid" id='but7' onClick={(event) => play(event.target)}> </button>
-      <button className="gamegrid" id='but8' onClick={(event) => play(event.target)}> </button>
+      <button className="gamegrid" id='but0' onClick={(event) => play(event.target)}></button>
+      <button className="gamegrid" id='but1' onClick={(event) => play(event.target)}></button>
+      <button className="gamegrid" id='but2' onClick={(event) => play(event.target)}></button><br></br>
+      <button className="gamegrid" id='but3' onClick={(event) => play(event.target)}></button>
+      <button className="gamegrid" id='but4' onClick={(event) => play(event.target)}></button>
+      <button className="gamegrid" id='but5' onClick={(event) => play(event.target)}></button><br></br>
+      <button className="gamegrid" id='but6' onClick={(event) => play(event.target)}></button>
+      <button className="gamegrid" id='but7' onClick={(event) => play(event.target)}></button>
+      <button className="gamegrid" id='but8' onClick={(event) => play(event.target)}></button>
     </div>
     <div className='reset-container'>
       <button id="reset" onClick={() => window.location.reload(false)}>reset</button>
